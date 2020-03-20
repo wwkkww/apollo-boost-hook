24beb4bd74ef938ec0540bec8f50521da66386a6
kevin <kevin@betacode.io>
2020-03-21 05:50:07 +0800

    init commit. Fetch post using useQuery hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import apolloClient from './apolloSetup';
+import Posts from './posts/Posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <ApolloProvider client={apolloClient}>
+    <Posts />
+  </ApolloProvider>
+);
 
 export default App;
+
